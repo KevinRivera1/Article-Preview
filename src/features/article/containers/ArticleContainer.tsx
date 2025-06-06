08afe1f05314effe7c20e94142921be97642af7c
KevinRivera1 <shimpo11.kr@gmail.com>
2025-06-06 17:13:12 -0500

    [FEAT](ArticleContainer): Add CardPreview Component

---
+++ src/features/article/containers/ArticleContainer.tsx
@@ -0,0 +1,9 @@
+import { CardPreview } from '../components/CardPreview';
+
+export const ArticleContainer = () => {
+	return (
+		<main className="mx-auto container grid place-content-center h-screen bg-rose-400">
+			<CardPreview />
+		</main>
+	);
+};
