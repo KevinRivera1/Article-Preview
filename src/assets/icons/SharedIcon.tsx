6f954bd445395bef2bc276509607e7a63e417a92
KevinRivera1 <shimpo11.kr@gmail.com>
2025-06-08 16:42:03 -0500

    [CHORE](SharedIcon): Remove stroke atributes unnecesaries

--- src/assets/icons/SharedIcon.tsx
+++ src/assets/icons/SharedIcon.tsx
@@ -11,9 +11,6 @@ export const SharedIcon = (props: SVGProps<SVGSVGElement>) => (
 		viewBox="0 0 20 20"
 		fill="none"
 		stroke="currentColor"
-		stroke-width="2"
-		stroke-linecap="round"
-		stroke-linejoin="round"
 		{...props}
 	>
 		<path fill="currentColor" d="M12 6V2l7 7l-7 7v-4c-5 0-8.5 1.5-11 5l.8-3l.2-.4A12 12 0 0 1 12 6" />
