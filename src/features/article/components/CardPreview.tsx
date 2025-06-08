72770a6ccc268a1c8565b71d02861488d7bb59a6
KevinRivera1 <shimpo11.kr@gmail.com>
2025-06-08 16:42:59 -0500

    [CHORE](CardPreview): improve styles

--- src/features/article/components/CardPreview.tsx
+++ src/features/article/components/CardPreview.tsx
@@ -3,7 +3,7 @@ import { Tooltip } from '../../../common/Tooltip';
 
 export const CardPreview = () => {
 	return (
-		<article className="w-[20rem] h-max bg-white overflow-hidden rounded-xl md:overflow-visible md:flex md:w-full md:h-max lg:w-[45rem] md:pb-0 shadow-2xl">
+		<article className="w-[21rem] h-max bg-white overflow-hidden rounded-xl md:overflow-visible md:flex md:w-full md:h-max lg:w-[45rem] md:pb-0 shadow-2xl">
 			<img
 				src="./image.png"
 				alt="Imagen de redes sociales"
@@ -11,11 +11,11 @@ export const CardPreview = () => {
 			/>
 			<div className="relative flex flex-col justify-center gap-10 px-8 py-2 mt-4 md:mt-0">
 				<div className="content flex flex-col gap-y-4">
-					<h1 className="font-bold text-pretty">
+					<h1 className="font-bold text-Very-Grayish-Blue text-pretty">
 						Shift the overall look and feel by adding these wonderful touches to forniture in your home
 					</h1>
 
-					<p className="text-pretty text-sm">
+					<p className="text-pretty font-medium text-Grayish-Blue text-sm">
 						Ever been in a room and felt like something was missing? Perhaps it feit slightly bare and uninviting. I've
 						got some simple tips to help you make any room feel complete.
 					</p>
@@ -23,8 +23,8 @@ export const CardPreview = () => {
 				<footer className="flex justify-between items-center py-2">
 					<img src="./perfil.webp" alt="imagen perfil de usuario" className="aspect-square rounded-full size-10" />
 					<div className="content-footer">
-						<span className="font-bold block">Michelle Appleton</span>
-						<span className="text-slate-400">28 jun 2025</span>
+						<span className="font-bold text-Very-Grayish-Blue block">Michelle Appleton</span>
+						<span className="text-Grayish-Blue font-medium">28 jun 2025</span>
 					</div>
 					<ButtonShared />
 				</footer>
