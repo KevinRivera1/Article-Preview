42b44a7ef731903588b854ce2e498d6459f79d38
KevinRivera1 <shimpo11.kr@gmail.com>
2025-06-08 17:19:31 -0500

    [CHORE](main.tsx): Add ShowContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { ShowContextProvider } from './contexts/ShowContextProvider.tsx';
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<ShowContextProvider>
+			<App />
+		</ShowContextProvider>
 	</StrictMode>,
 );
